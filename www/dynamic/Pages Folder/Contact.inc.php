23e9d029fb0a94a3ad14a1e741f2e80fd2fc9aa0
Rijwan9365480654 <rijwan9365480654@gmail.com>
2022-02-02 17:01:34 +0000

    Dynamic Project Complete

---
+++ www/dynamic/Pages Folder/Contact.inc.php
@@ -0,0 +1,138 @@
+<h1 id="request">Movie Premier Booking Form</h1>
+<p class="req">Interested in Movie Premier at NY Cinema? Please complete and submit the following form to the Booking Office. One of our representatives will send you an information package tailored to the field(s) of Premier that interest you. Please indicate whether you would like additional information or not</p>
+
+
+<style type="text/css">
+    input[type="text"],
+    input[type="email"],
+    textarea {
+        border: 1px solid dashed;
+        background-color: rgb(221, 216, 212);
+        width: 480px;
+        padding: .5em;
+        font-size: 1.0em;
+    }
+
+    .Error {
+        color: red;
+        font-size: 1.2em;
+        font-family: Bitter, Georgia, Times, "Times New Roman", serif;
+    }
+
+    input[type="submit"] {
+        color: white;
+        float: right;
+        font-size: 1.3em;
+        font-family: Bitter, Georgia, Times, "Times New Roman", serif;
+        width: 170px;
+        height: 40px;
+        background-color: #5D0580;
+        border: 5px solid;
+        border-bottom-left-radius: 35px;
+        border-bottom-right-radius: 35px;
+        border-top-left-radius: 35px;
+        border-top-right-radius: 35px;
+        border-color: rgb(221, 216, 212);
+        font-weight: bold;
+    }
+
+    .FieldInfo {
+        color: rgb(251, 174, 44);
+        font-family: Bitter, Georgia, "Times New Roman", Times, serif;
+        font-size: 1.3em;
+
+
+    }
+
+    .MF {
+        color: black;
+        font-size: 1.2em;
+        font-family: Bitter, Georgia, Times, "Times New Roman", serif;
+    }
+</style>
+
+<?php
+if (isset($_POST["Submit"])) {
+    if (empty($_POST["Name"])) {
+        $NameError = "(Name is Required)";
+    } else {
+        $Name = Test_User_Input($_POST["Name"]);
+        if (!preg_match("/^[A-Za-z. ]*$/", $Name)) {
+            $NameError = "(Only Letters and WhiteSpaces are allowed)";
+        }
+    }
+
+    if (empty($_POST["Email"])) {
+        $EmailError = "(Email is Required)";
+    } else {
+        $Email = Test_User_Input($_POST["Email"]);
+        if (!preg_match("/[A-Za-z0-9._-]{3,}@[A-Za-z0-9._-]{3,}[.]{1}[A-Za-z0-9._-]{2,}/", $Email)) {
+            $EmailError = "(Invalid Email Format)";
+        }
+    }
+
+    if (empty($_POST["Gender"])) {
+        $GenderError = "(Gender is Required)";
+    } else {
+        $Gender = Test_User_Input($_POST["Gender"]);
+    }
+
+    if (empty($_POST["Website"])) {
+        $WebsiteError = "(Website is Required)";
+    } else {
+        $Website = Test_User_Input($_POST["Website"]);
+        if (!preg_match("/(https:|ftp:)\/\/+[a-zA-Z0-9.\-_\/?\$=&\#\~`!]+\.[A-Za-z0-9.\-_\/?\$=&\#\~`!]*/", $Website)) {
+            $WebsiteError = "(Invalid Website Format)";
+        }
+    }
+}
+function Test_User_Input($Data)
+{
+    return $Data;
+}
+
+if (!empty($_POST["Name"]) && !empty($_POST["Email"]) && !empty($_POST["Gender"]) && !empty($_POST["Website"])) {
+    if (preg_match("/^[A-Za-z. ]*$/", $Name) == true && preg_match("/[A-Za-z0-9._-]{3,}@[A-Za-z0-9._-]{3,}[.]{1}[A-Za-z0-9._-]{2,}/", $Email) && preg_match("/(https:|ftp:)\/\/+[a-zA-Z0-9.\-_\/?\$=&\#\~`!]+\.[A-Za-z0-9.\-_\/?\$=&\#\~`!]*/", $Website)) {
+        echo "<h2><u>Your  Submitted Information</u></h2>";
+        echo "Name: {$_POST["Name"]}<BR>";
+        echo "Email: {$_POST["Email"]}<BR>";
+        echo "Gender: {$_POST["Gender"]}<BR>";
+        echo "Website: {$_POST["Website"]}<BR>";
+        echo "Comment: {$_POST["Comment"]}<BR>";
+    } else {
+        echo '<span class="Error">(Correct The Form & Submit Again)<br></span>';
+    }
+} else {
+    echo '<span class="Error">(Fill all the Fields with *)<br></span>';
+}
+
+?>
+
+<form action="" method="post">
+    <legend>* Please Fill Out the following Fields.</legend>
+    <fieldset>
+        <span class="FieldInfo">
+            Name:</span><br>
+        <input class="input" type="text" Name="Name" value="">
+        <span class="Error">*<?php echo $NameError;  ?></span><br>
+        <span class="FieldInfo">
+            E-mail:</span><br>
+        <input class="input" type="text" Name="Email" value="">
+        <span class="Error">*<?php echo $EmailError; ?></span><br>
+        <span class="FieldInfo">
+            Gender:</span><br>
+        <input class="radio" type="radio" Name="Gender" value="Female"><span class="MF">Female</span>
+        <input class="radio" type="radio" Name="Gender" value="Male"><span class="MF">Male</span>
+        <span class="Error">*<?php echo $GenderError; ?></span><br>
+        <span class="FieldInfo">
+            Website:</span><br>
+        <input class="input" type="text" Name="Website" value="">
+        <span class="Error">*<?php echo $WebsiteError; ?></span><br>
+        <span class="FieldInfo">
+            Comment:</span><br>
+        <textarea Name="Comment" rows="5" cols="25"></textarea>
+        <br>
+        <br>
+        <input type="Submit" Name="Submit" value="Submit">
+    </fieldset>
+</form>
---
+++ www/dynamic/Pages Folder/ShareWithFriend.inc.php
@@ -0,0 +1,75 @@
+<div>
+    <section class="action info">
+        <h2>Action Movies</h2>
+        <img src="images/action.jpg" width="290" height="250" alt="action">
+
+
+    </section>
+    <section class="comedy info">
+        <h2>Comedy Movies</h2>
+        <img src="images/bean.jpg" width="290" height="250" alt="action">
+
+    </section>
+    <section class="animation info">
+        <h2>Animated Movies</h2>
+        <img src="images/34.jpg" width="290" height="250" alt="action">
+
+    </section>
+    <section class="upcoming info">
+        <h2>Upcoming Movies</h2>
+        <img src="images/upcoming.jpg" width="290" height="250" alt="action">
+
+    </section>
+</div>
+<div class="clear"></div>
+<h1 class="contenthead">Follow Us</h1>
+<div id="social">
+    <ul>
+        <li style="margin-left: -20px;"><a href="https://www.facebook.com/jazeb.akram" target="_blank"><img src="images/fb.png" alt="facebook" /></a></li>
+        <li><a href="https://twitter.com/jazebakram" target="_blank"><img src="images/tweet.png" alt="tweeter" /></a></li>
+        <li><a href="https://plus.google.com/+jazebakram/posts" target="_blank"><img src="images/google.png" alt="G+" /></a></li>
+        <li><a href="https://www.youtube.com/channel/UC6x8G3Vw2I9FQBPJeOCpkSA" target="_blank"><img src="images/youtube.png" alt="youtube" /></a></li>
+        <li><a href="https://www.linkedin.com/in/jazebakram" target="_blank"><img src="images/link.png" width="110px" height="110px;" alt="linkdIn" /></a></li>
+    </ul>
+</div>
+
+<div class="clear"></div>
+<div class="clear"></div>
+
+</div>
+
+<!-- Side Area -->
+<div id="side">
+    <section class="links">
+        <h3>Latest Movie Links</h3>
+        <ul>
+            <li><a href="#">Action Movies</a></li>
+            <li><a href="#">Animated Movies</a></li>
+            <li><a href="#">Comedy Movies</a></li>
+            <li><a href="#">Drama Movies</a></li>
+            <li><a href="#">Horror Movies</a></li>
+
+        </ul>
+    </section>
+    <h2 style="background: url(images/pattern.png);">Today's Top Movies</h2>
+    <section class="sideimage">
+        <img src="images/V.jpg" width="270" height="120" alt="" />
+
+        <h4 style="background: url(images/pattern.png);">V for Vandetta</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <a href="#">More</a>
+        </p>
+    </section>
+    <br>
+    <div class="clear"></div>
+    <section class="sideimage">
+        <img src="images/jokerthegreat.jpg" width="270" height="120" alt="" />
+
+        <h4 style="background: url(images/pattern.png);">Joker</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <a href="#">More</a>
+        </p>
+    </section>
+
+    </p><br><br><br><br><br></section>
+    <div class="clear"></div>
+    <br>
+</div>
